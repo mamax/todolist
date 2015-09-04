1f490ddf5a996b2aec323d6b4481a8a0f23e6280
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-09-04 14:02:38 +0300

    full_title and rspec test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'SOME TODO LIST'" do
+      visit root_path
+      expect(page).to have_content('SAMPLE TODO LIST')
+    end
+
+    it "should have the title 'Home'" do
+      visit root_path
+      expect(page).to have_title("MyTodoList | Home")
+    end
+  end
+end
